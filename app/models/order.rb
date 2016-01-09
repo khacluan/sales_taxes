2f0d40c67ef5b77965d7108258ba67f1eb0cbcc4
Luan D <luan@lixibox.com>
2016-01-09 12:54:55 +0700

    refactore code

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,7 +2,11 @@ class Order < ActiveRecord::Base
   has_many :order_products, dependent: :destroy
   has_many :products, through: :order_products
 
-  def subtotal
-    order_products.map {|op| op.product.price * op.quantity }.sum
+  def total
+    order_products.sum(:price)
+  end
+
+  def tax_price
+    products.map(&:tax_price).reject(&:nil?).sum
   end
 end
